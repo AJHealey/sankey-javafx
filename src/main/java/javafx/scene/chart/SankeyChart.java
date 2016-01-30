54de22bc1157523c3650fae046cb6af0e1993cc1
Adrian Healey <adrian.j.healey@gmail.com>
2016-01-30 20:45:37 +0100

    Implements sankey chart data structure

--- src/main/java/javafx/scene/chart/SankeyChart.java
+++ src/main/java/javafx/scene/chart/SankeyChart.java
@@ -1,7 +1,51 @@
 package javafx.scene.chart;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Adrian Healey <adrian.j.healey@gmail.com>
  */
-public class SankeyChart {
+public class SankeyChart extends Chart{
+
+    private double nodeWidth = 24;
+    private double nodePadding = 8;
+
+    private Set<SankeyNode> nodes;
+    private Set<SankeyLink> links;
+
+    public SankeyChart() {
+        nodes = new HashSet<SankeyNode>();
+        links = new HashSet<SankeyLink>();
+    }
+
+    public Set<SankeyNode> getNodes() {
+        return nodes;
+    }
+
+    public Set<SankeyLink> getLinks() {
+        return links;
+    }
+
+    public double getNodePadding() {
+        return nodePadding;
+    }
+
+    public void setNodePadding(double nodePadding) {
+        this.nodePadding = nodePadding;
+    }
+
+    public double getNodeWidth() {
+        return nodeWidth;
+    }
+
+    public void setNodeWidth(double nodeWidth) {
+        this.nodeWidth = nodeWidth;
+    }
+
+    @Override
+    protected void layoutChartChildren(double top, double left, double width, double height) {
+
+    }
+
 }
---
+++ src/main/java/javafx/scene/chart/SankeyNode.java
@@ -0,0 +1,16 @@
+package javafx.scene.chart;
+
+/**
+ * @author Adrian Healey <adrian.j.healey@gmail.com>
+ */
+public class SankeyNode {
+    private String name;
+
+    public SankeyNode(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
