6c06e4e72b175ace131e20259319b7230114e0bc
Adrian Healey <adrian.j.healey@gmail.com>
2016-01-31 19:33:08 +0100

    Implement equals, hashcode and row field

--- src/main/java/javafx/scene/chart/SankeyNode.java
+++ src/main/java/javafx/scene/chart/SankeyNode.java
@@ -7,6 +7,7 @@ public class SankeyNode {
     private String name;
     private double value = 0.0;
     private int column = 0;
+    private int row = 0;
 
     public SankeyNode(String name) {
         this.name = name;
@@ -34,5 +35,33 @@ public class SankeyNode {
 
     void moveRight() {
         this.column++;
+    }
+
+    int getRow() {
+        return row;
+    }
+
+    void setRow(int row) {
+        this.row = row;
+    }
+
+    void movDown() {
+        this.row++;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SankeyNode that = (SankeyNode) o;
+
+        return name.equals(that.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
     }
 }
